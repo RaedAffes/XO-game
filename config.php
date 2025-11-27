b975ca6529c84c47b1f0a2ac98325bc216a94544
RaedAffes <raedaffes@gmail.com>
2025-11-27 22:29:01 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
 // config.php - Fichier de configuration sécurisé
 define('DB_HOST', 'sql301.infinityfree.com');
 define('DB_NAME', 'if0_40529673_game');        // Change si ton préfixe est différent
-define('DB_USER', 'if0_40529673');
-define('DB_PASS', 'B3nNa1wvkMNlqX');          // Change ce mot de passe dans ton vrai compte !
+define('DB_USER', '***********');
+define('DB_PASS', '***************');          // Change ce mot de passe dans ton vrai compte !
 
 // Pour plus de sécurité, tu peux ajouter .htaccess pour bloquer l'accès direct
 // (voir fichier .htaccess plus bas)
+
 ?>
